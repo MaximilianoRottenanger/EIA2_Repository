2116d6353b24f34b6dfa891d37204cec31a08aea
MaximilianoRottenanger <maxi002@gmx.de>
2024-12-14 15:54:49 +0100

    Weitergeführt und Activity Diagram Hochgeladen

---
+++ L09.1_OldMacDonaldsFarm/main.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Stock = exports.Food = exports.Animal = void 0;
+let animals;
+let stock;
+window.addEventListener("load", handleLoad);
+function handleLoad() {
+    let cow = new Animal("Cow", "hay", "Muh!", 7);
+    animals.push(cow);
+    let sheep = new Animal("Sheep", "grass", "Meeeh!", 2);
+    animals.push(sheep);
+    let pig = new Animal("Pig", "garbage", "Oink!", 5);
+    animals.push(pig);
+    let fox = new Animal("Fox", "Catfood", "bark!", 2);
+    animals.push(fox);
+    let pigeon = new Animal("Pigeon", "Breadcrums", "Gurru!", 10);
+    stock = new Stock();
+    stock;
+    createSong();
+}
+class Animal {
+    animalName;
+    food;
+    sound;
+    amount;
+    constructor(_name, _food, _sound, _amount) {
+        this.animalName = _name;
+        this.food = _food;
+        this.sound = _sound;
+        this.amount = _amount;
+    }
+    sing() { }
+    eat() { }
+}
+exports.Animal = Animal;
+class Food {
+    foodName;
+    amount;
+    constructor(_name, _amount) {
+        this.foodName = _name;
+        this.amount = _amount;
+    }
+}
+exports.Food = Food;
+class Stock {
+    array;
+    constructor(_array) {
+        this.array = _array;
+    }
+    adjustFood(_name) { }
+    ;
+}
+exports.Stock = Stock;
+function createSong() {
+}
+//# sourceMappingURL=main.js.map
---
+++ L09.1_OldMacDonaldsFarm/main.ts
@@ -0,0 +1,65 @@
+let animals: Animal[];
+let stock: Stock;
+
+window.addEventListener("load", handleLoad);
+
+function handleLoad(): void {
+    let cow: Animal = new Animal("Cow", "hay", "Muh!", 7);
+    animals.push(cow);
+
+    let sheep: Animal = new Animal("Sheep", "grass", "Meeeh!", 2);
+    animals.push(sheep);
+
+    let pig: Animal = new Animal("Pig", "garbage", "Oink!", 5);
+    animals.push(pig);
+
+    let fox: Animal = new Animal("Fox", "Catfood", "bark!", 2);
+    animals.push(fox);
+
+    let pigeon: Animal = new Animal("Pigeon", "Breadcrums", "Gurru!", 10);
+
+    stock = new Stock() stock
+
+    createSong();
+}
+
+
+export class Animal {
+    animalName: string;
+    food: string;
+    sound: string;
+    amount: number;
+
+    constructor(_name: string, _food: string, _sound: string, _amount: number) {
+        this.animalName = _name;
+        this.food = _food;
+        this.sound = _sound;
+        this.amount = _amount;
+    }
+    sing(): void { }
+    eat(): void { }
+}
+
+export class Food {
+    foodName: string;
+    amount: string;
+
+    constructor(_name: string, _amount: string) {
+        this.foodName = _name;
+        this.amount = _amount;
+    }
+}
+
+
+export class Stock {
+    array: Food;
+
+    constructor(_array: Food) {
+        this.array = _array;
+    }
+    adjustFood(_name): void { };
+}
+
+function createSong(): void{
+    
+}
